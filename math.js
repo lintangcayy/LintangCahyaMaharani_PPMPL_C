9ed3e1cfdf4bc9dd0a683ae186a6b75c0301d451
unknown <chylintang212@gmail.com>
2024-10-08 00:51:22 +0700

    P1

--- math.js
+++ math.js
@@ -1,39 +1,39 @@
 export function tambah(a, b) {
-  if (typeof a !== "number" || typeof b !== "number") {
-    throw new Error("Input harus berupa angka");
-  }
   if (a == null || b == null) {
     throw new Error("Input tidak boleh null atau undefined");
+  }
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("Input harus berupa angka");
   }
   return a + b;
 }
 
 export function kali(a, b) {
-  if (typeof a !== "number" || typeof b !== "number") {
-    throw new Error("Input harus berupa angka");
-  }
   if (a == null || b == null) {
     throw new Error("Input tidak boleh null atau undefined");
+  }
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("Input harus berupa angka");
   }
   return a * b;
 }
 
 export function kurang(a, b) {
-  if (typeof a !== "number" || typeof b !== "number") {
-    throw new Error("Input harus berupa angka");
-  }
   if (a == null || b == null) {
     throw new Error("Input tidak boleh null atau undefined");
+  }
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("Input harus berupa angka");
   }
   return a - b;
 }
 
 export function bagi(a, b) {
-  if (typeof a !== "number" || typeof b !== "number") {
-    throw new Error("Input harus berupa angka");
-  }
   if (a == null || b == null) {
     throw new Error("Input tidak boleh null atau undefined");
+  }
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("Input harus berupa angka");
   }
   if (b === 0) {
     throw new Error("Tidak bisa membagi dengan nol");
